24de971f4f929b57d1d92da4ca13ae255ee0a78f
kelyAna <kely.lopes@vtex.com>
2024-01-05 16:46:02 -0300

    feat: scalar types

--- queries-and-resolvers/src/index.js
+++ queries-and-resolvers/src/index.js
@@ -1,1 +1,28 @@
-console.log('oi, vamos estudar!');
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      id: ID!
+      name: String!
+      age: Int!
+      average: Float!
+      married: Boolean!
+      arrayString: [String!]!
+    }
+  `,
+  resolvers: {
+    Query: {
+      id: () => '123ew',
+      name: () => 'Ana',
+      age: () => 23,
+      average: () => 2.3,
+      married: () => false,
+      arrayString: () => ['A', 'B'],
+    },
+  },
+});
+
+server.listen(4003).then(({ url }) => {
+  console.log(`server listening on url ${url}`);
+});
