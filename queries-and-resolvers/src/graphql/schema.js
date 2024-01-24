56113f6d889c4a35bfcbf6c287b5b35278327013
kelyAna <kely.lopes@vtex.com>
2024-01-24 20:53:04 -0300

    feat: code improvements

--- queries-and-resolvers/src/graphql/schema.js
+++ queries-and-resolvers/src/graphql/schema.js
@@ -4,6 +4,7 @@ import { postResolvers } from './post/resolvers';
 import { postTypeDefs } from './post/typedefs';
 import { userResolvers } from './user/resolvers';
 import { userTypeDefs } from './user/typedefs';
+import { apiFiltersResolvers } from './api-filters/resolvers';
 
 const rootTypeDefs = gql`
   type Query {
@@ -23,4 +24,9 @@ export const typeDefs = [
   postTypeDefs,
   apiFiltersTypeDefs,
 ];
-export const resolvers = [rootResolvers, userResolvers, postResolvers];
+export const resolvers = [
+  rootResolvers,
+  userResolvers,
+  postResolvers,
+  apiFiltersResolvers,
+];
